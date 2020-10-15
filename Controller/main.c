4bb7c9a6c0a5fda4cc26d7e30b8fda5be11367f7
Justin-Vuong <vuong.justin01@gmail.com>
2020-10-14 23:54:47 -0400

    Added joystick logic for both motors on an H-bridge
    git pull merge
    
    Update main.c
    
    Updated comment formatting
    
    Update main.c
    
    Updated comments format pt2

--- Controller/main.c
+++ Controller/main.c
@@ -13,15 +13,16 @@ https://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcont
 double dutyCycle = 0;
 
 void start_conversion(){
+
 	ADCSRA |= (1 << ADSC); 		//Start a read on the ADC
+
 }
 
 void ADC_setup(){
 	//Using a 10 bit ADC
-	ADMUX = (1 << REFS0) | (1 << MUX0) | (1 << MUX2) | (1 << ADLAR); 					//Use external voltage as reference, use ADC5, left adjust register to read only 8 MSB
+	ADMUX = (1 << REFS0) | (1 << MUX0) | (1 << MUX2) | (1 << ADLAR); 			//Use external voltage as reference, use ADC5, left adjust register to read only 8 MSB
 	ADCSRA = (1 << ADEN) | (1 << ADIE) | (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2); 	//Enable ADC, Enable interrupts, set prescalar
-	DIDR0 = (1 << ADC5D); 																//Disable digital input buffer to avoid reading wrong value 
-
+	DIDR0 = (1 << ADC5D); 									//Disable digital input buffer to avoid reading wrong value 
 	start_conversion();
 }
 
@@ -30,15 +31,15 @@ int main(void)
 
 	DDRD = (1 << PORTD5) | (1 << PORTD6);			//Setting pin D6 as output
 	TCCR0A = (1 << COM0A1) | (1 << COM0B1);			//Setting timer to clear OC0A and OC0B pin on compare match and set pin after overflow
-	TCCR0A |= (1 << WGM00) | (1 << WGM01);	  		//Set Fast PWM with timer overflow at 0xFF
-	TIMSK0 = (1 << TOIE0);					  		//Trigger interrupt upon timer overflow
+	TCCR0A |= (1 << WGM00) | (1 << WGM01);	 		//Set Fast PWM with timer overflow at 0xFF
+	TIMSK0 = (1 << TOIE0);					//Trigger interrupt upon timer overflow
 
 	ADC_setup();
 
 	sei(); 		//Enable external interrupts
 
 	TCCR0B = (1 << CS00) | (1 << CS02); 		//Setting prescalar to 1024. Setting this bit starts the timer
-
+	
 	while (1)
 	{
 		//Application code
@@ -49,7 +50,8 @@ int main(void)
 
 //Configure external interupt to update duty cycle upon overflow
 ISR(TIMER0_OVF_vect){
-	OCR0A = dutyCycle; //Set compare registers value
+	//Set compare registers values
+	OCR0A = dutyCycle; 
 	OCR0B = dutyCycle;
 }
 
@@ -57,6 +59,7 @@ ISR(ADC_vect)
 {
 	//The joystick's control of the PWM must be mirror around the center 
 	//as the direction of the wheels is controlled by setting pins. 
+
 	dutyCycle = ADCH;
 	
 	if (dutyCycle - 127 > 0) {
